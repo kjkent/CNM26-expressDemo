e3a82d3f6e0752b617e5105ce2742434cd34a7c6
kjkent <kris@kriskent.com>
2021-09-01 23:40:45 +0100

    Added first route

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 1024;
+
+// Express takes two objects, a request and a response
+app.get("/", (req, res) => {
+    //status refers to HTTP status codes 1xx info, 2xx success, 3xx redirects, 4xx client error, 5xx server error
+    res.status(200).send("Hello world");
+});
+
+app.listen(port, () => {
+    console.log("App is online");
+});
